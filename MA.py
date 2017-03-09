d4997dcf410badec5c7b9b931316d12898ca1a55
Martin Martin <mcm@fb.com>
2017-03-09 05:21:42 -0800

    Refactor to separate node from tree structure.
    This makes the two different types of equality -- same node vs same
    subtree -- more natural to express, and allows functions that return
    other functions, e.g. f(x)(y).

--- MA.py
+++ MA.py
@@ -1,112 +1,95 @@
-# Sage symbolic boolean expressions don't mix with sage symbolic
-# algebra expressions.  And it doesn't seem to have first order logic
-# constructs like "for all."  It seems really poor for the theorem
-# proving stuff I'm trying to do.  So I'm ditching Sage.  Should
-# probably go with Mathematica, but will stay with a general
-# programming language as long as possible.
-
-
-# Not sure we need a base class for all expression types, but what the heck.
 class Expression:
-    def __init__(self, *args):
-        # Not sure this should be in here.  Oh well.
-        self.children = args
-
     def __mul__(self, rhs):
-        return Multiply(self, rhs)
+        return CompositeExpression([Multiply(), self, rhs])
 
+    # "+" already means "concatenate" for lists.  But concatenating
+    # argument lists seems much rarer than constructing expressions
+    # using "+", especially in abstract algebra.
     def __add__(self, rhs):
-        return Sum(self, rhs)
-
-    # Overriding this is a little dangerous, since Python assumes it
-    # returns a bool to test whether two Expressions are equal in a
-    # lot of places.  But Sage seems to do it, so we'll see how much
-    # trouble this gets us in.
-    def __eq__(self, rhs):
-        return Equals(self, rhs)
-
-    def __ne__(self, rhs):
-        # For now, no three valued logic.
-        return Not(self == rhs)
+        return CompositeExpression([Sum(), self, rhs])
 
 
-class Infix(Expression):
-    def __init__(self, name, *args):
-        Expression.__init__(self, *args)
+class CompositeExpression(Expression, list):
+    # I'm using inheritence instead of composition.  Oh well.
+    def __repr__(self):
+        assert(len(self) > 0)
+        return self[0].repr_tree(self[1:])
+
+
+class Node(Expression):
+    # Mathematica calls this an Atom.  In Mathematica, Head of a node is
+    # its type.  Seems non-orthogonal.
+
+    def repr_tree(self, args):
+        return repr(self) + '(' + ', '.join([repr(arg) for arg in args]) + ')'
+
+
+class Infix(Node):
+    def __init__(self, name):
         self.name = name
         self.name_with_spaces = ' ' + name + ' '
 
-    def __repr__(self):
-        return "(" + self.name_with_spaces.join([str(child)
-                                                 for child in self.children]) + ")"
-
-
-class Prefix(Expression):
-    def __init__(self, name, *args):
-        Expression.__init__(self, *args)
-        self.name = name
-
-    def __repr__(self):
-        return self.name + \
-            "(" + ', '.join([str(child) for child in self.children]) + ")"
+    def repr_tree(self, args):
+        return "(" + self.name_with_spaces.join([str(arg)
+                                                 for arg in args]) + ")"
 
 
 class Multiply(Infix):
-    def __init__(self, *args):
-        Infix.__init__(self, '*', *args)
+    def __init__(self):
+        Infix.__init__(self, '*')
 
 
 class Sum(Infix):
-    def __init__(self, *args):
-        Infix.__init__(self, '+', *args)
+    def __init__(self):
+        Infix.__init__(self, '+')
 
 
 class Element(Infix):
-    def __init__(self, *args):
-        Infix.__init__(self, r'\in', *args)
+    def __init__(self):
+        Infix.__init__(self, r'\in')
 
 
 class Equivalent(Infix):
-    def __init__(self, left, right):
-        Infix.__init__(self, r'\iff', left, right)
+    def __init__(self):
+        Infix.__init__(self, r'\iff')
 
 
 class Implies(Infix):
-    def __init__(self, left, right):
-        Infix.__init__(self, r'\implies', left, right)
+    def __init__(self):
+        Infix.__init__(self, r'\implies')
 
 
 class And(Infix):
-    def __init__(self, *args):
-        Infix.__init__(self, 'and', *args)
+    def __init__(self):
+        Infix.__init__(self, 'and')
 
 
 class Or(Infix):
-    def __init__(self, *args):
-        Infix.__init__(self, 'or', *args)
+    def __init__(self):
+        Infix.__init__(self, 'or')
 
 
-class Not(Prefix):
-    def __init__(self, *args):
-        Infix.__init__(self, 'not', *args)
+class Not(Node):
+    def __repr__(self):
+        return 'not'
 
 
 class Equals(Infix):
-    def __init__(self, *args):
-        Infix.__init__(self, '==', *args)
+    def __init__(self):
+        Infix.__init__(self, '==')
 
 
-class ForAll(Prefix):
-    def __init__(self, vars_, expr):
-        Prefix.__init__(self, r'\forall', vars_, expr)
+class ForAll(Node):
+    def __repr__(self):
+        return r'\forall'
 
 
-class Exists(Prefix):
-    def __init__(self, *args):
-        Prefix.__init__(self, r'\exists', *args)
+class Exists(Node):
+    def __repr__(self):
+        return r'\exists'
 
 
-class Variable(Expression):
+class Variable(Node):
     def __init__(self, name):
         self.name = name
 
@@ -114,16 +97,32 @@ class Variable(Expression):
         return self.name
 
 
+def makefn(clz, name=''):
+    def maker(*args):
+        return CompositeExpression([clz()] + list(args))
+    maker.__name__ = name if name else clz.__name__.lower()
+    return maker
+
+
+equals = makefn(Equals)
+forall = makefn(ForAll)
+implies = makefn(Implies)
+equivalent = makefn(Equivalent)
+element = makefn(Element)
+and_ = makefn(And, 'and_')
+or_ = makefn(Or, 'or_')
+
+
 def var(name):
     return Variable(name)
 
 
-def Iff(left, right):
-    return Equivalent(left, right)
+def iff(left, right):
+    return equivalent(left, right)
 
 
-def In(left, right):
-    return Element(left, right)
+def in_(left, right):
+    return element(left, right)
 
 
 
@@ -141,14 +140,14 @@ Q = var('Q')
 R = var('R')
 
 # Multiplication is distributive
-leftDist = ForAll([P, Q, R], R * (P + Q) == R * P + R * Q)
-rightDist = ForAll([P, Q, R], (P + Q) * R == P * R + Q * R)
+leftDist = forall([P, Q, R], equals(R * (P + Q), R * P + R * Q))
+rightDist = forall([P, Q, R], equals((P + Q) * R, P * R + Q * R))
 
 # This is the definition of B:
-defB = ForAll(P, Iff(In(P, B), P * M == M * P))
+defB = forall(P, iff(in_(P, B), equals(P * M, M * P)))
 
 # This is what we have to prove:
-toprove = ForAll([P, Q], Implies(And(In(P, B), In(Q, B)), In(P + Q, B)))
+toprove = forall([P, Q], implies(and_(in_(P, B), in_(Q, B)), in_(P + Q, B)))
 
 print(leftDist)
 print(rightDist)
@@ -167,10 +166,53 @@ print(toprove)
 # (plus other premises) to the end.  In fact, the start may be a red
 # herring, so for now we can focus on the end.
 
+# We need some pattern matching.
+#
+# We start with P + Q \in B, and want to find all rules which match it.
+#
+# This is where overriding == might be killing me?  Well, I think I
+# need two diffferent kinds of equals, "node equals" and "tree
+# equals," the second one recurses.  But for testing whether a node is
+# in dummies, it would be really helpful to use "in".  In general,
+# we'll want a hash table from nodes to all expressions which contain
+# that node.  All this suggests having node objects that don't have
+# children, and having a separate list structure to put them all
+# together.  Like LISP and Mathematica...  But that's not object
+# oriented!  :) Well, maybe we don't want object oriented for the
+# structure?  If we're doing pattern matching, the tree structure is
+# pretty fundamental.  And I think all of mathematics can fit in the
+# tree structure?  For example, does LaTeX make that assumption?  Of
+# course, LaTeX only has to display math, it doesn't include
+# semantics, so it can probably cheat.
+#
+# What is node_eq used for?  Pattern matching, and that's about it I
+# guess.  So am I driving my design based on a special case?
+# Mathematica allows the "head" (what I'm calling the node) to
+# actually be a subtree.  This allows you to write f(x)(y).  In a
+# traditional programming language, you'd have a "function call" node
+# in the AST, so you'd also end up with the function being a child of
+# that node.  Ok, let's do that then.
+
 
 # Random Design Notes
 #
 #############
+# Not Using Sage
+#
+# Sage symbolic boolean expressions don't mix with sage symbolic
+# algebra expressions.  And it doesn't seem to have first order logic
+# constructs like "for all."  It doesn't do much symbolic manipulation
+# on its own, instead it hands expressions off to other packages like
+# Maxima to do the symbol manipulation.  It seems to have a different
+# representation for every subdomain, and you can't create trees that
+# mix them.  In general, it seems really poor for the theorem proving
+# stuff I'm trying to do.  So I'm ditching Sage.  Should probably go
+# with Mathematica, but will stay with a general programming language
+# as long as possible.
+#
+#############
+# How To Indicate Pattern Match (universally quantified) variables
+#
 # Explicit scoping of free variables, vs. Mathematica's trailing
 # underscore, versus having a different type (e.g. var('P') declares
 # generic variables that are implcitly forall at the start of any
